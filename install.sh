14b8d3b0a19e2af549c307e7061e7ebff73dbd4b
NekoSuneVR <Chisdeal2014@hotmail.com>
2025-05-27 18:22:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -227,7 +227,7 @@ check_network
 display_menu
 
 # Handle user selection and installation
-install "ubuntu" "Ubuntu"
+install "debian" "Debian"
 
 # Copy run.sh script to ROOTFS_DIR and make it executable
 cp /run.sh "$ROOTFS_DIR/run.sh"
